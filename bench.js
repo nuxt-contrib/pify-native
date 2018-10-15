48625ac0fa7cfdcc7ae24fd4056d4df3b7957ff3
Pooya Parsa <pyapar@gmail.com>
2018-10-15 22:54:15 +0330

    refactor bench.js

--- bench.js
+++ bench.js
@@ -1,42 +1,45 @@
 const Benchmark = require('benchmark');
-const pkg = require('./package.json')
+const pkg = require('./package.json');
 
-console.log(pkg.name + '@' + pkg.version)
-console.log()
-console.log('Node: ' + process.versions.node)
-console.log('V8: ' + process.versions.v8)
-console.log()
+console.log(pkg.name + '@' + pkg.version);
+console.log();
+console.log('Node: ' + process.versions.node);
+console.log('V8: ' + process.versions.v8);
+console.log();
 
 const pify = require('.');
 
 const suite = new Benchmark.Suite();
 
 const cases = [
-  cb => cb(),
-  cb => cb(new Error()),
-  (cb) => cb(JSON.stringify({ val: Math.random() })),
-  (cb) => setImmediate(cb(Math.random()))
+	cb => cb(),
+	cb => cb(new Error()),
+	cb => cb(JSON.stringify({val: Math.random()})),
+	cb => setImmediate(cb(Math.random()))
 ];
 
-const nativeCases = () => cases.map(c => pify(c, { native: true }));
-const nonNativeCases = () => cases.map(c => pify(c, { native: false }));
+const nativeCases = () => cases.map(c => pify(c, {native: true}));
+const nonNativeCases = () => cases.map(c => pify(c, {native: false}));
 
-const createDifferedTest = (cases) => {
-  return {
-    defer: true,
+const createDifferedTest = cases => {
+	return {
+		defer: true,
     fn(deferred) {
-      Promise
-        .all(cases().map(c => c().catch(e => e)))
-        .then(() => deferred.resolve())
-    }
-  }
-}
+      // Prevent inlining
+      suite.name;
+
+			Promise
+        .all(cases().map(c => c().catch(error => error)))
+				.then(() => deferred.resolve());
+		}
+	};
+};
 
 suite
-  .add('native', createDifferedTest(nativeCases))
-  .add('non-native', createDifferedTest(nonNativeCases))
-  .on('cycle', event => console.log(String(event.target)))
-  .on('complete', function () {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
-  })
-  .run();
+	.add('native', createDifferedTest(nativeCases))
+	.add('non-native', createDifferedTest(nonNativeCases))
+	.on('cycle', event => console.log(String(event.target)))
+	.on('complete', function () {
+		console.log('Fastest is ' + this.filter('fastest').map('name'));
+	})
+	.run();
